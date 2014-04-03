d654cf0958ffc5c17d32c03cd6725c5b695bd787
Jack Bowen <jbowen02@calpoly.edu>
2014-04-03 14:08:40 -0700

    Restoring repository

---
+++ configureGit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script configures git whenever you start working on multiple machines. Run it any time you decide to work with git on a new computer.
+git config --global user.name "Jack Bowen"
+git config --global user.email "jbowen02@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
+
